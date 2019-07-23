84930fa8322c7189f2037065cda5223f983dd922
Niklas <niklas.schmidt44@gmail.com>
2019-07-23 21:43:16 +0200

    modified install.sh, i think

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 git pull --force
 
@@ -8,3 +8,8 @@ pip3 install requests
 read -p "Please download https://www.datendieter.de/item/Liste_von_deutschen_Staedtenamen_.csv as staedte_osm.txt || Press Enter to continue"
 read -p "Please download https://www.datendieter.de/item/Liste_von_deutschen_Strassennamen_.csv as strassen_osm.txt || Press Enter to continue"
 
+read -sp "Please input your bot token: " token
+echo ""
+read -p "Please input you discord user ID: " uid
+
+echo '{"token": "'$token'", "owner_id": "'$uid'"}' > config.json
