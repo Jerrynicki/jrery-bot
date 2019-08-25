12ad0751d3b5a1d9d4255eca1de3cb8aa0696379
Niklas <niklas.schmidt44@gmail.com>
2019-08-25 18:42:23 +0200

    trogi kack gefixt

--- events/on_message.py
+++ events/on_message.py
@@ -9,5 +9,14 @@ class OnMessageEvent(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if random.randint(1, 300) == 150:
-            await message.channel.send("<:trogi:615175220256309261>")
+            trogi_emote = None
+            for emote in message.guild.emojis:
+                print(emote.name)
+                if emote.name == "trogi":
+                    trogi_emote = emote
+
+            if trogi_emote = None:
+                return
+
+            await message.channel.send("<:" + trogi_emote.name + ":" + str(trogi_emote.id) + ">")
             return
